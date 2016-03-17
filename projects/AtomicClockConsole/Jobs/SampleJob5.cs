a646da77a84513f4654062744f691dfc5b6362ad
Nima Shahri <NShahri@users.noreply.github.com>
2016-03-18 11:00:26 +1300

    set up samples

---
+++ projects/AtomicClockConsole/Jobs/SampleJob5.cs
@@ -0,0 +1,15 @@
+﻿namespace AtomicClock.WinService.Jobs
+{
+    using System;
+    using System.Threading;
+
+    public class SampleJob5
+    {
+        public static void DoJob(dynamic data)
+        {
+            Console.WriteLine("Started: {0} - {1}", data, DateTime.Now);
+            Thread.Sleep(2000);
+            Console.WriteLine("Done:    {0} - {1}", data, DateTime.Now);
+        }
+    }
+}
--- projects/AtomicClockConsole/Program.cs
+++ projects/AtomicClockConsole/Program.cs
@@ -1,10 +1,5 @@
 ﻿namespace AtomicClock.WinService
 {
-    using System;
-    using System.Collections.Concurrent;
-    using System.Threading;
-    using System.Threading.Tasks;
-
     using Topshelf;
 
     internal class Program
@@ -28,32 +23,6 @@
                         s.WhenStopped(tc => tc.Stop());
                     });
             });
-
-            //using (BlockingCollection<int> bc = new BlockingCollection<int>())
-            //{
-
-            //    // Kick off a producer task
-            //    Task.Factory.StartNew(() =>
-            //    {
-            //        for (int i = 0; i < 10; i++)
-            //        {
-            //            bc.Add(i);
-            //            Thread.Sleep(5000); // sleep 100 ms between adds
-            //        }
-
-            //        // Need to do this to keep foreach below from hanging
-            //        bc.CompleteAdding();
-            //    });
-
-            //    // Now consume the blocking collection with foreach.
-            //    // Use bc.GetConsumingEnumerable() instead of just bc because the
-            //    // former will block waiting for completion and the latter will
-            //    // simply take a snapshot of the current state of the underlying collection.
-            //    foreach (var item in bc.GetConsumingEnumerable())
-            //    {
-            //        Console.WriteLine(item);
-            //    }
-            //}
         }
     }
 }
